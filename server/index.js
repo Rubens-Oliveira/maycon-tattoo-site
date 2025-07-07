437d9280828f65a7e5f6bd40bbf6ef2670a2ba75
Rubens <rubenscordeiromg@gmail.com>
2025-07-07 09:28:08 -0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,25 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const sql = require('mssql');
 const app = express();
+const PORT = 3001;
 
-const agendamentoRoutes = require('./routes/agendamento');
-const adminRoutes = require('./routes/admin');
+app.get('/', (req, res) => {
+  res.send('Servidor Backend está rodando.');
+});
 
-const config = {
-  user: 'seu_usuario',
-  password: 'sua_senha',
-  server: 'localhost',
-  database: 'MayconTattoo',
-  options: { trustServerCertificate: true }
-};
-
-sql.connect(config).then(() => console.log('Conectado ao SQL Server')).catch(err => console.error(err));
-
-app.use(cors());
-app.use(express.json());
-app.use('/', agendamentoRoutes);
-app.use('/', adminRoutes);
-
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
